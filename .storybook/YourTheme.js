b21d36f1005ba8cffa2e97779777abb14a82f048
Georgios Georgiou <georgios.georgiou@newcrosshealthcare.com>
2023-05-02 21:56:17 +0300

    feat: theme changes, font styles, tmp scss button fix

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,8 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+  fontBase: "Inter",
+  brandTitle: "CityJS Workshop",
+  brandTarget: "_self",
+});
