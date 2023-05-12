ab6a5d7a5b3c5950fc8b06b361c2fe806d3cc622
Georgios Georgiou <georgios.georgiou@newcrosshealthcare.com>
2023-05-12 20:02:33 +0300

    feat: storybook bump 7.0.11, minor theme

--- .storybook/YourTheme.js
+++ .storybook/YourTheme.js
@@ -2,7 +2,7 @@ import { create } from "@storybook/theming/create";
 
 export default create({
   base: "light",
-  fontBase: "Inter",
+  fontBase: '"Inter", sans-serif',
   brandTitle: "CityJS Workshop",
   brandTarget: "_self",
 });
