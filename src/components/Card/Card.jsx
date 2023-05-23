359d508859e24678679b88332b191927d64bc9fa
Dimitris Afendras <dimitris.afendras@newcrosshealthcare.com>
2023-05-23 21:10:01 +0300

    fix bug

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -15,9 +15,6 @@ export const Card = ({ className, children, ...props }) => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
         </Typography>
       </div>
       <Button>Button Label</Button>
