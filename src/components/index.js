1be95e8784d1d9222020fd45a2a28c9b8d26eaf1
Dimitris Afendras <dimitris.afendras@newcrosshealthcare.com>
2023-05-31 18:00:46 +0300

    fixes

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@ export { Button } from "./Button";
 export { Card } from "./Card";
 export { Header } from "./Header";
 export { Typography } from "./Typography";
-export { Sidebar } from "./Sidebar";
 export { Main } from "./Main";
